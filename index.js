cb8a813f8badc5a9d2147ee014e4ce6651f7e2e6
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-04-11 23:55:47 +0200

    Add callback to the function

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var rect = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
-}
+const rectangle = require('./rectangle');
+var rect = require('./rectangle');
 
 function solveReact(l, b){
     console.log("Solving for rectangle with l = "+ l +" and b = "+ b);
+    rect(l, b, (err, rectangle) => {
+        if(err){
+            console.log("ERROR: ", err.message);
+        }
+        else{
+            console.log("Area is: "+ rectangle.area());
+            console.log("Perimeter is: "+ rectangle.perimeter())
+        }
 
-    if(l <= 0 || b <= 0){
-        console.log("Rectangle dimensions should be greater than zero: l = "+ l +" and b = "+ b);
-    }
-    else{
-        console.log("The area of the rectangle is: "+ rect.area(l, b));
-        console.log("The perimeter of the rectangle is: "+ rect.perimeter(l, b));
-    }
+    });
 }
 
 solveReact(2, 4);
 solveReact(3, 5);
+solveReact(0, 5);
