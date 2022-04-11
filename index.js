591d3b32b565c1a2dfcb16e6bb8006c94e94ded6
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-04-12 01:11:22 +0200

    Setting up the express server

--- index.js
+++ index.js
@@ -1,80 +1,24 @@
+const express = require('express');
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-var rect = require('./rectangle');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log("Request for "+ req.url +" by method "+ req.method);
+const app = express();
 
-    if(req.method == 'GET'){
-        var fileUrl;
-        if(req.url == '/') fileUrl = '/index.html';
-        else fileUrl = req.url;
+app.use((req, res, next) => {
+    console.log(req.headers);
 
-        var filePath = path.resolve('./public'+ fileUrl);
-        const fileExt = path.extname(filePath);
-
-        if(fileExt == '.html'){
-            fs.exists(filePath, (exists) => {
-                if (!exists) {
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type', 'text/html');
-                    res.end(`<html><body>
-                    Error 404: ${ fileUrl } not found
-                     </body></html>`);
-
-                     return;
-                }
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                fs.createReadStream(filePath).pipe(res);
-            });
-        }
-        else{
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`<html><body>
-            Error 404: ${ fileUrl } not an HTML file
-             </body></html>`);
-    
-             return;
-        }
-        
-    }
-    else{
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`<html><body>
-        Error 404: ${ req.method } not supported
-         </body></html>`);
-
-         return;
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<html><body>
+        <h1>This is an Express Server</h1>
+     </body></html>`);
 });
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log(`Server is running at http://${ hostname }:${ port }`);
 });
-
-
-
-
-
-
-
-function solveReact(l, b){
-    console.log("Solving for rectangle with l = "+ l +" and b = "+ b);
-    rect(l, b, (err, rectangle) => {
-        if(err){
-            console.log("ERROR: ", err.message);
-        }
-        else{
-            console.log("Area is: "+ rectangle.area());
-            console.log("Perimeter is: "+ rectangle.perimeter())
-        }
-
-    });
-}
