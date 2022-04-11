34bf78b8489ff3e5b3745df92c8f4ee65b9aa04e
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-04-12 01:18:03 +0200

    Setting express server to use morgan and server static pages

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+// Setting express to use static files saved in the public folder
+app.use(express.static(__dirname +"/public"));
 
 app.use((req, res, next) => {
-    console.log(req.headers);
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
