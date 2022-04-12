cb3e1403b9c39ca1ab7caf36bd1281805781f9b9
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-04-12 02:15:30 +0200

    Applying dish endpoint to the server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3001;
 
@@ -10,51 +11,25 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-    console.log(req.body);
-    // Send back the response to the server
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    //res.send(`This is an Express Server using Body Parser`);
-    next();
-});
+// app.get('/dishes/:dishId', (req, res, next) => {
+//     res.end(`Will send details of the dish: ${ req.params.dishId } to you`);
+// });
 
-app.get('/dishes', (req, res, next) => {
-    res.end('Send all the dishes to you!');
-});
+// app.post('/dishes/:dishId', (req, res, next) => {
+//     res.end('POST operation no supported on /dishes/'+ req.params.dishId);
+// });
 
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: '+ req.body.name +' with details: '+ 
-    req.body.description);
-});
+// app.put('/dishes/:dishId', (req, res, next) => {
+//     res.write('Updating the dish: '+ req.params.dishId); 
+//     res.end('Will update the dish: '+ req.body.name +
+//     ' with details: '+ req.body.description);
+// });
 
-app.put('/dishes', (req, res, next) => {
-    res.end('PUT operation no supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all the dishes to you!');
-});
-
-
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end(`Will send details of the dish: ${ req.params.dishId } to you`);
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.end('POST operation no supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: '+ req.params.dishId); 
-    res.end('Will update the dish: '+ req.body.name +
-    ' with details: '+ req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: '+ req.params.dishId);
-});
+// app.delete('/dishes/:dishId', (req, res, next) => {
+//     res.end('Deleting dish: '+ req.params.dishId);
+// });
 // Setting express to use static files saved in the public folder
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname +"/public"));
 
 app.use((req, res, next) => {
