12acfb43eb2555bc6afb9cc2a368af2c423405fc
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-04-12 02:01:16 +0200

    Adding body parser to express and rest api end to the server

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = 3001;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
+app.all('/dishes', (req, res, next) => {
+    console.log(req.body);
+    // Send back the response to the server
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    //res.send(`This is an Express Server using Body Parser`);
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: '+ req.body.name +' with details: '+ 
+    req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.end('PUT operation no supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes to you!');
+});
+
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end(`Will send details of the dish: ${ req.params.dishId } to you`);
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.end('POST operation no supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: '+ req.params.dishId); 
+    res.end('Will update the dish: '+ req.body.name +
+    ' with details: '+ req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: '+ req.params.dishId);
+});
 // Setting express to use static files saved in the public folder
 app.use(express.static(__dirname +"/public"));
 
